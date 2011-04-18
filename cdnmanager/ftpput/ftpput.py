ef4ab34e0db033da13b9875a98c70008f216906e
Thales <chacara@gmail.com>
2011-04-18 02:05:00 +0000

    implementing new modifications to the command line parser

--- cdnmanager/ftpput/ftpput.py
+++ cdnmanager/ftpput/ftpput.py
@@ -1,89 +1,107 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+__autor__ = "Thales Chácara <chacara@gmail.com>"
+__date__ = "16/04/2011 16:50"
 
-from ftplib import FTP
-import os, sys, getopt
+"""
+description:
 
-def upload(srv, usr, pwd, dir_local, arquivo, dir_remoto):
+    a simple testing tool, just to send files for the 
+    ftpserver automatically. ftpput uses the module ftputil, 
+    is a high level interface to the module ftplib. 
+"""
+
+import ftputil, os, sys, getopt
+
+def usage():
+    print '''usage: ftpput.py -s <hostname> -u <user> -p <passwd> -l <video-file> -t <target-dir> \n
+arguments:
+        -s, --server      (hostname or ip address)
+        -u, --user        (username)
+        -p, --passwd      (password)
+        -l, --video-file  (local video file)
+        -t, --target-dir  (put video file in remote directory)
+        
+Video Delivery Network <http://www.vdeli.com.br>'''
+
+def put(hostname, username, passwd, local, target, mode):
+    """    
+    performs upload videos to FTP server.
+    
+    return:
+        0 - upload completed.
+        1 - could not establish connection with server.
+        2 - video has not been sent.
     """
-    Executa upload dos arquivos para o servidor FTP.
-
-    Retorno:
-        [ 0 ] Upload Concluido.
-        [ 1 ] Não foi possivel estabelecer uma conexao com o servidor.
-        [ 2 ] Arquivo inexistente.
-        [ 3 ] Nao foi possivel autenticar-se com o usuario e senha fornecidos.
-        [ 4 ] Nao foi possivel enviar arquivo desejado.
-
-    """
-    os.chdir(dir_local)
-
+    
     try:
-        ftp = FTP(srv,)
+        ftp = ftputil.FTPHost(hostname, username, passwd) 
     except:
-        return 1
-
-    if not os.path.exists(arquivo):
+        return 1 
+    
+    try:
+        ftp.makedirs(target, mode=None)
+        video = local.split('/')[-1]
+        target = target + '/' + video        
+        ftp.upload(local, target, mode, callback=None)
+    except:
         return 2
-
-    try:
-        ftp.login(usr, pwd)
-    except:
-        return 3
-
-    try:
-        ftp.cwd(dir_remoto)
-        ftp.storbinary('STOR ' + arquivo, file(arquivo))
-    except:
-        return 4
-     
+    
     return 0
 
 
-#ftp cliente
-
-
-saidas = ["upload concluido.", 
-         "[ Errno: 1 ] nao foi possivel estabelecer uma conexao com o servidor.",
-         "[ Errno: 2 ] arquivo inexistente.",
-         "[ Errno: 3 ] nao foi possivel autenticar-se com o usuario e senha fornecidos.",
-         "[ Errno: 4 ] nao foi possivel enviar arquivo desejado."]
-
-
 def main(argv):
-    srv = ''
-    usr = ''
-    pwd = ''
-    dir_local = '/tmp' 
-    arquivo = ''
-    dir_remoto = '/'
-    try:
-         opts, args = getopt.getopt(argv, "h:s:u:p:dl:a:dr", ["help", "server", "user", "passwd", "dirlocal", "arquivo", "dirremoto"])
-    except getopt.GetoptError:
-        print "ftpput.py -s <server> -u <user> -p <passwd>"
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '':
-            print "ftpput.py -s <server> -u <user> -p <passwd>"
-            sys.exit(1)
-        elif opt in ("-s", "--server"):
-            srv = arg
-        elif opt in ("-u", "--user"):
-            usr = arg
-        elif opt in ("-p", "--passwd"):
-            pwd = arg
-        elif opt in ("-dl", "--dirlocal"):
-            dir_local = arg
-        elif opt in ("-a", "--arquivo"):
-            arquivo = arg
-        elif opt in ("-dr", "--dirremoto"):
-            dir_remoto = arg 
+    """    
+    provides support for parsing command line.
     
-    print "server: ", srv
-    print "user: ", usr
-    resultado = upload(srv, usr, pwd, dir_local, arquivo, dir_remoto) 
-    print saidas[resultado]
+    return:
+        0 - upload completed.
+        1 - could not establish connection with server.
+        2 - video has not been sent.
+    """
+    try: 
+        opts, args = getopt.getopt(argv, "h:s:u:p:l:t:a", ["help",
+                                                           "server",
+                                                           "username",
+                                                           "passwd",
+                                                           "video-file",
+                                                           "target-dir"]
+                                   )
+    except getopt.GetoptError, error:
+        print str(error)
+        usage()
+        sys.exit(2)
+    
+    # (default transfer mode: binary)
+    mode = "b"
+    
+    for option, argument in opts:
+        if option == "-a":
+            mode = "a"
+        elif option in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif option in ("-s", "--server"):
+            hostname = argument
+        elif option in ("-u", "--username"):
+            username = argument
+        elif option in ("-p", "--passwd"):
+            passwd = argument
+        elif option in ("-l", "--video-file"):
+            local = argument
+        elif option in ("-t", "--target-dir"):
+            target = argument
+        else:
+            assert False, "unhandled option"
+
+    output = put(hostname, username, passwd, local, target, mode)
+    
+    messages = ["upload completed.",
+                "could not establish connection with server.",
+                "video has not been sent."
+                ]
+    print messages[output]
 
 
 if __name__ == "__main__":
