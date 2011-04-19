db4bfebac24175c197b8a594d4580992b68e4583
Thales <chacara@gmail.com>
2011-04-19 02:00:03 +0000

    modifications that provide code execution as a linux daemon, creating a dependency of the module pypid

--- cdnmanager/ftpserver/ftpserver.py
+++ cdnmanager/ftpserver/ftpserver.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-from sqlite3.test.userfunctions import authorizer_cb
+#
+# Init file for ftpserver server daemon
+#
+# chkconfig: 2345 99 99
+# description: ftpserver initialization to system structure based on redhat
 
-__autor__ = "chácara"
+__autor__ = "Thales Chácara <chacara@gmail.com>"
+__date__ = "16/04/2011 16:11"
+
+"""
+description:
+
+    this is a simple FTP server based on pyftpdlib, which is a great library
+    that supports almost everything you need for a working FTP. It checks the 
+    user authorization in a user library stored in a database, that is managed
+    by the cdnmanager component.
+"""
 
 from pyftpdlib import ftpserver
 import sqlite3
+import pypid
 
-authorizer = ftpserver.DummyAuthorizer()
+ftpserver_daemon = pypid.Daemon()
 
-db = sqlite3.connect('ftpusers.db')
-cur = db.cursor()
-cur.execute('select * from users')
-
-for user in cur:
-    authorizer.add_user(user[0], user[1], "/srv/git/vdeli/ftpd", perm="elradfmw")
-
-authorizer.add_anonymous("/")
-handler = ftpserver.FTPHandler
-handler.authorizer = authorizer
-address = ("0.0.0.0", 21)
-ftpd = ftpserver.FTPServer(address, handler)
-ftpd.serve_forever()
+if __name__ == "__main__":
+    ftpserver_daemon.args(stdout='/var/log/ftpserver.log', 
+                          pidfile='/var/run/ftpserver.pid')
+    def ftpserverd():
+        authorizer = ftpserver.DummyAuthorizer()
+   
+        db = sqlite3.connect("/srv/git/vdeli/cdnmanager/ftpserver/ftpusers.db")
+        cur = db.cursor()
+        cur.execute('select * from users')
+        
+        for user in cur:
+            authorizer.add_user(user[0], user[1], \
+                                "/srv/git/vdeli/cdnmanager/ftpserver", perm="elradfmw")
+        
+        authorizer.add_anonymous("/")
+        handler = ftpserver.FTPHandler
+        handler.authorizer = authorizer
+        address = ("0.0.0.0", 21)
+        ftpd = ftpserver.FTPServer(address, handler)
+        ftpd.serve_forever()
+    
+    ftpserverd()
