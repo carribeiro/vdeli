889ff6f7434c66a047285132d9bb729d41115aa7
Alexey Kostyuk <unitoff@gmail.com>
2011-04-20 16:58:21 +0400

    Updated fabric script Updated gitignore file Added small bug fixes to the settings.py

--- cdnmanager/cdnmanager/cdn/settings.py
+++ cdnmanager/cdnmanager/cdn/settings.py
@@ -1,5 +1,7 @@
 # Django settings for cdn project.
 
+import os
+
 def rel(path):
     return os.path.join(os.path.abspath(os.path.dirname(__file__)), path)
 
--- cdnmanager/fabfile.py
+++ cdnmanager/fabfile.py
@@ -5,15 +5,15 @@ from contextlib import contextmanager as _contextmanager
 
 # globals
 env.prj_name = 'vdeli' # no spaces!
-env.webserver = 'nginx' # nginx or apache2 (directory name below /etc!)
-env.dbserver = 'mysql' # mysql or postgresql
+env.webserver = 'apache2' # nginx or apache2 (directory name below /etc!)
+env.dbserver = 'postgresql' # mysql or postgresql
 
 # environments
 
-def localhost(path='/home/%(user)s/PROJECTS' % env):
+def localhost(path='/home/%(user)s/PROJECTS' % env,user='vdeliadmin'):
     "Use the local virtual server"
     env.hosts = ['localhost']
-    env.user = 'vdeliadmin'
+    env.user = user
     env.path = '%s/%s' % (path,env.prj_name)
     env.virtualenv_path = '%(path)s/.env' % env
     env.activate = 'source %(virtualenv_path)s/bin/activate' % env
@@ -27,6 +27,15 @@ def cdnmanager():
     env.virtualhost_path = '%s/virtualenv/vdeli' % env.path
     env.pysp = '%(virtualhost_path)s/lib/python2.6/site-packages' % env
     env.tmppath = '/var/tmp/django_cache/%(prj_name)s' % env
+
+def test(path='/home/%(user)s/PROJECTS' % env,user='vdeliadmin'):
+    "Test server"
+    env.hosts = ['10.7.1.5'] # Change to your server name!
+    env.user = user
+    env.path = '%s/%s' % (path,env.prj_name)
+    env.virtualenv_path = '%(path)s/.env' % env
+    env.activate = 'source %(virtualenv_path)s/bin/activate' % env
+    env.use_local_devel_machine = True
 
 @_contextmanager
 def virtualenv():
@@ -44,6 +53,9 @@ def setup():
     
     # for localhost we don't need the webserver
     if env.use_local_devel_machine:
+        
+        sudo('apt-get install gcc python-all-dev libpq-dev git-core -y')
+        
         if not exists('/usr/bin/virtualenv',use_sudo=True):
             sudo('apt-get install python-virtualenv -y')
         if not exists('/usr/bin/pip',use_sudo=True):
@@ -62,6 +74,7 @@ def setup():
 South
 django-extensions
 pyftpdlib
+psycopg2
 """
         requirements.write(requirements_data)
         requirements.close()
@@ -71,15 +84,27 @@ pyftpdlib
         with virtualenv():
             sudo('pip install -r %(path)s/requirements.txt' % env,user=env.user)
 
+        sudo('apt-get install -y postgresql')
+        sudo('psql -c "CREATE USER %s WITH NOCREATEDB NOCREATEUSER ENCRYPTED PASSWORD E\'%s\'"' % ('cdnmanager', 'CdnManager'), user='postgres')
+        sudo('psql -c "CREATE DATABASE %s WITH OWNER %s"' % (
+            'cdn', 'cdnmanager'), user='postgres')
+        
+#        with cd(env.path):
+#            sudo('git clone git@github.com:carribeiro/vdeli.git')
+
     else:
         # install webserver and database server
         sudo('apt-get remove -y apache2 apache2-mpm-prefork apache2-utils') # is mostly pre-installed
+        # install gcc C compiler
+        sudo('apt-get install gcc -y')
         if env.webserver=='nginx':
             sudo('apt-get install -y nginx')
         else:
             sudo('apt-get install -y apache2-mpm-worker apache2-utils') # apache2-threaded
             sudo('apt-get install -y libapache2-mod-wsgi') # outdated on hardy!
-        if env.dbserver=='mysql':
-            sudo('apt-get install -y mysql-server python-mysqldb')
-        elif env.dbserver=='postgresql':
-            sudo('apt-get install -y postgresql python-psycopg2')
+
+        sudo('apt-get install -y postgresql git git-core')
+        sudo('psql -c "CREATE USER %s WITH NOCREATEDB NOCREATEUSER ENCRYPTED PASSWORD E\'%s\'"' % ('cdnmanager', 'CdnManager'), user='postgres')
+        sudo('psql -c "CREATE DATABASE %s WITH OWNER %s"' % (
+            'cdn', 'cdnmanager'), user='postgres')
+
