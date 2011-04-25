2328f8477f2482c4942c9be897f89afebbbbea18
chacara <chacara@gmail.com>
2011-04-24 22:04:00 -0700

    Ticket # 15 entering the banner

--- cdnmanager/local_scripts/ftpconfig.py
+++ cdnmanager/local_scripts/ftpconfig.py
@@ -1,4 +1,6 @@
 # basic ftpserver config
 import os.path
 
+FTP_VERSION = "0.1 alpha"
+FTP_BANNER = "VDN FTP Server %s ready" % FTP_VERSION
 FTP_HOME_DIR = os.path.join("_VDELIHOME_", "cdnmanager/cdnmanager/cdn/uploads")
