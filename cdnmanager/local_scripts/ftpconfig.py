046e832d968e283637f46471a9d1e6bc05448706
chacara <chacara@gmail.com>
2011-04-24 21:12:32 -0700

    fix FTP_HOME_DIR path

--- cdnmanager/local_scripts/ftpconfig.py
+++ cdnmanager/local_scripts/ftpconfig.py
@@ -1,4 +1,4 @@
 # basic ftpserver config
 import os.path
 
-FTP_HOME_DIR = os.path.join("_VDELIHOME_", "cdnmanager/cdnmanger/cdn/uploads")
+FTP_HOME_DIR = os.path.join("_VDELIHOME_", "cdnmanager/cdnmanager/cdn/uploads")
