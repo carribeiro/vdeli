3b70cec38d103025a423250a8c1fce69bab1596c
Carlos Ribeiro <carribeiro@gmail.com>
2011-04-25 02:47:58 -0300

    Added the test_celery_task script, which is a simple test script which connects to the Django server using celery and sends a videofile.import call with fixed arguments

---
+++ cdnmanager/ftpserver/test_celery_task.py
@@ -0,0 +1,14 @@
+# simple test against celery
+
+from celery.task.control import inspect
+i = inspect()
+print i.registered_tasks()
+
+from celery.execute import send_task
+#r = send_task("videofile.import", ['/home/cribeiro/work/vdeli/cdnmanager/cdnmanager/cdn/uploads/cribeiro/myproject/lixao'])
+r = send_task("videofile.import", ['/home/cribeiro/work/vdeli/cdnmanager/cdnmanager/cdn/uploads/cribeiro/myproject/lixao1'])
+print r
+import time
+for i in xrange(30):
+    print i, r.ready(), r.result
+    time.sleep(1)
