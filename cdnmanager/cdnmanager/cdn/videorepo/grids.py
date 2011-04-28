0a537eb2fcae369de5cfef32c53aa460dbe2bbd3
Carlos Ribeiro <carribeiro@gmail.com>
2011-04-28 02:11:27 -0300

    Fixed #43. Tried to implement a calculated column on the queryset but it didn't work

--- cdnmanager/cdnmanager/cdn/videorepo/grids.py
+++ cdnmanager/cdnmanager/cdn/videorepo/grids.py
@@ -2,6 +2,7 @@ from djangojqgrid.jqgrid import JqGrid
 from django.core.urlresolvers import reverse
 from videorepo.models import VideoFile
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
 
 def VideoFilesByUserGrid(user=None):
     """ Class/instance factory. See ticket #23. 
@@ -21,9 +22,13 @@ def VideoFilesByUserGrid(user=None):
 
         #fields = ['upload_date', 'file_name', 'file_hash', 'file_size'] # optional 
         url = reverse('video_files_by_user_grid_handler')
-        caption = 'Video files by user' # optional
+        caption = '_(Video files by user)' # optional
+        fields = ('project__name', 'file_name', 'file_size', 'file_hash',  'upload_date')
         colmodel_overrides = {
-            'id': { 'editable': True, 'width':10 },
+            'project__name': { 'width': 80 },
+            #file_name': { 'name': 'File name' },
+            'file_size': { 'align':'right', 'editable': False, 'width':30 },
+            'upload_date': { 'align':'center', 'width': 60 }
         }
 
     # returns an instance of the class
