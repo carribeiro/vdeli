e5f4372028ed8c1570fe5ca670ac1a7215396d0c
chacara <chacara@gmail.com>
2011-04-27 22:28:00 -0700

    applying the fix suggested in Issue #44

--- cdnmanager/local_scripts/ftpconfig.py
+++ cdnmanager/local_scripts/ftpconfig.py
@@ -3,4 +3,5 @@ import os.path
 
 FTP_VERSION = "0.1 alpha"
 FTP_BANNER = "VDN FTP Server %s ready" % FTP_VERSION
+FTP_AUTH_ADDRESS = "_AUTH_SERVER_"
 FTP_HOME_DIR = os.path.join("_VDELIHOME_", "cdnmanager/cdnmanager/cdn/uploads")
